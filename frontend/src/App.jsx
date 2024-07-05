adac05bd6ac5b3c0b13fbfb65c6201869b98470e
Apurva Kumar <apurvakumar0313@gmail.com>
2024-07-06 00:11:53 +0530

    Implemented chakra ui for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css";
 import { Route } from "react-router-dom";
 import HomePage from "./Components/Pages/HomePage";
 import ChatPage from "./Components/Pages/ChatPage";
--- frontend/src/Components/Pages/HomePage.jsx
+++ frontend/src/Components/Pages/HomePage.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import { Container, Box, Text, Center } from "@chakra-ui/react";
 const HomePage = () => {
-  return <div>Home</div>;
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p="2"
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="3xl" fontFamily="Work Sans" textAlign="center">
+          Baatein Karo
+        </Text>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
